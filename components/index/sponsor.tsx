3280ff40c5f6854f9d47963bf8de2a4dd0ae7966
ichiro16go <ichi1.9adgjm@akane.waseda.jp>
2026-03-06 12:16:58 +0900

    スポンサーロゴ小さい

--- components/index/sponsor.tsx
+++ components/index/sponsor.tsx
@@ -1,28 +1,29 @@
 import SectionHeading from "./section-heading";
 import Image from "next/image";
 
-
 export default function Sponsors() {
     return (
         <>
-            <div className="divide-effect flex flex-col" >
-                <section className="gsap-trigger border-t border-b border-foreground/10 w-full max-w-7xl py-16 lg:py-32 relative">
+            <div className="divide-effect flex flex-col">
+                <section className="gsap-trigger border-t border-b border-foreground/10 w-full max-w-7xl py-16 lg:py-32 relative mx-auto">
                     <SectionHeading titleEn="Affiliated with" titleJa="スポンサー様" />
 
-                    <Image
-                        src="/affiliates/gmo-pepabo.svg"
-                        width={200}
-                        height={31}
-                        alt="GMOペパボ株式会社"
-                        className="p-0.5"
-                    />
-                    <Image
-                        src="/affiliates/progate.png"
-                        width={200}
-                        height={31}
-                        alt="Progate株式会社"
-                        className="p-0.5"
-                    />
+                    <div className="mt-12 flex flex-col items-center justify-center gap-12 md:flex-row md:gap-24">
+                        <Image
+                            src="/affiliates/gmo-pepabo.svg"
+                            width={400}
+                            height={62}
+                            alt="GMOペパボ株式会社"
+                            className="h-auto w-auto max-w-[300px] md:max-w-[400px] object-contain"
+                        />
+                        <Image
+                            src="/affiliates/progate.png"
+                            width={400} 
+                            height={62}
+                            alt="Progate株式会社"
+                            className="h-auto w-auto max-w-[300px] md:max-w-[400px] object-contain"
+                        />
+                    </div>
                 </section>
             </div>
         </>
