5efab0e3d8520b149a578ccf7ec33f80d3eb7589
ichiro16go <ichi1.9adgjm@akane.waseda.jp>
2026-03-07 13:53:39 +0900

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import { Instagram } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center px-4 gap-16 bg-gradient-to-b from-transparent to-background divide-effect">
-      <section className="flex flex-col lg:flex-row justify-around items-center w-full max-w-7xl text-center lg:text-end py-6 border-b border-gray-400">
+      {/* <section className="flex flex-col lg:flex-row justify-around items-center w-full max-w-7xl text-center lg:text-end py-6 border-b border-gray-400">
         <p className="text-sm font-semibold p-3 text-gray-600 w-[-webkit-fill-available] lg:w-auto font-mono">
           Affiliated with
         </p>
@@ -24,7 +24,7 @@ export default function Footer() {
           alt="Progate株式会社"
           className="p-0.5"
         />
-      </section>
+      </section> */}
       <section className="flex flex-col lg:flex-row gap-8 lg:gap-16 justify-between w-full max-w-7xl lg:py-16 px-4 lg:px-0 lg:pr-3">
         <Link href="/">
           <Image
