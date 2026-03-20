61dab5499aaeafc4e56cd8812d025961f9f6ce48
Sota Matsui <sota.matsui@akane.waseda.jp>
2026-03-20 18:12:05 +0900

    fix: 背景が黒くなる問題を修正

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,46 +15,23 @@ export default async function Home() {
   const projects = await getAllProjects();
 
   return (
-    <>
-      <div>
-        <div id="homepage-background" className="fixed h-screen w-screen -z-1"></div>
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script id="script">
-          {`VANTA.NET({
-              el: "#homepage-background",
-              mouseControls: true,
-              touchControls: true,
-              gyroControls: false,
-              minHeight: 200.00,
-              minWidth: 200.00,
-              scale: 1.00,
-              scaleMobile: 1.00,
-              color: 0x69ff,
-              backgroundColor: 0x0
-            });`}
-        </Script>
-      </div>
-      <main className="flex flex-col items-center">
-        <PageHeader titleJa="プロジェクト一覧" titleEn="Our.projects();" desc="WINCアプリチームの制作実績と、進行中のプロジェクトをお伝えします" />
-        <section className="w-full flex flex-col items-center divide-effect bg-background">
-          <div className="w-full max-w-[1440px] flex flex-row items-stretch relative">
-            <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
-            <div className="w-full max-w-7xl">
-              <div className="min-h-[80vh] divide-effect flex flex-col justify-start">
-                <ProjectSection projects={projects} />
-              </div>
+    <main className="flex flex-col items-center">
+      <PageHeader
+        titleJa="プロジェクト一覧"
+        titleEn="Our.projects();"
+        desc="WINCアプリチームの制作実績と、進行中のプロジェクトをお伝えします"
+      />
+      <section className="w-full flex flex-col items-center divide-effect bg-background">
+        <div className="w-full max-w-[1440px] flex flex-row items-stretch relative">
+          <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
+          <div className="w-full max-w-7xl">
+            <div className="min-h-[80vh] divide-effect flex flex-col justify-start">
+              <ProjectSection projects={projects} />
             </div>
-            <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
           </div>
-        </section>
-      </main>
-    </>
+          <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
+        </div>
+      </section>
+    </main>
   );
 }
